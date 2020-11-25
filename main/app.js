f247cd567a3794c4a96bbc2cc644739772605b4c
unknown <adrianabiza14@gmail.com>
2020-11-25 21:05:21 +0200

    todos

--- main/app.js
+++ main/app.js
@@ -1,5 +1,27 @@
 function addTokens(input, tokens){
+    if(typeof(input)!=='string')
+        throw 'Invalid input';
+        
+    if(input.length < 6)
+        throw 'Input should have at least 6 characters';
+
+    if(Array.isArray(tokens)){ 
+        for(let i=0;i<tokens.length;i++){
+            if(tokens[i].tokenName===undefined || typeof(tokens[i].tokenName)!=='string'){
+                throw 'Invalid array format';
+            }
+        }
+    }    
     
+    if(!input.includes("...")){
+        return input;
+    }
+
+    for (let i = 0; i < tokens.length; i++) {
+        input = input.replace("...", '${'+tokens[i].tokenName+'}');
+    }
+    return input;
+
 }
 
 const app = {
